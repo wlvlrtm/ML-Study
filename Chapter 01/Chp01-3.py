1b83352a0039d1dcb838ce22f71d25e1b94c4a48
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2022-09-16 00:23:29 +0900

    Chapter 01, 02 Update

---
+++ Chapter 01/Chp01-3.py
@@ -0,0 +1,66 @@
+from hashlib import new
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+def main() :
+    ## 도미 데이터
+    bream_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0, 
+                    31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0, 
+                    35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0]
+    bream_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 500.0, 475.0, 500.0, 
+                    500.0, 340.0, 600.0, 600.0, 700.0, 700.0, 610.0, 650.0, 575.0, 685.0, 620.0, 680.0, 
+                    700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0]
+
+    ## 빙어 데이터
+    smelt_length = [9.8, 10.5, 10.6, 11.0, 11.2, 11.3, 11.8, 11.8, 12.0, 12.2, 12.4, 13.0, 14.3, 15.0]
+    smelt_weight = [6.7, 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
+
+    ## 도미 산점도
+    plt.scatter(bream_length, bream_weight)
+    plt.xlabel("Length")
+    plt.ylabel("Weight")
+    plt.show()
+
+    ## 빙어 산점도
+    plt.scatter(smelt_length, smelt_weight)
+    plt.xlabel("Length")
+    plt.ylabel("Weight")
+    plt.show()
+
+    ## 도미 + 빙어
+    fish_length = (bream_length + smelt_length)
+    fish_weight = (bream_weight + smelt_weight)
+    fish_data = [[l, w] for l, w in zip(fish_length, fish_weight)]
+
+    plt.scatter(bream_length, bream_weight)
+    plt.scatter(smelt_length, smelt_weight)
+    plt.xlabel("Length")
+    plt.ylabel("Weight")
+    plt.show()
+
+    ## 정답 데이터
+    answer_book = [1] * 35 + [0] * 14
+
+    ## 모델 훈련
+    kn = KNeighborsClassifier()
+    kn.fit(fish_data, answer_book)
+    kn.score(fish_data, answer_book)
+
+    ## 새로운 데이터 판별
+    new_fish = [30, 600]    # len: 30, wei: 600
+    print(kn.predict([new_fish]))
+
+    ## 최종 산점도
+    plt.scatter(bream_length, bream_weight)
+    plt.scatter(smelt_length, smelt_weight)
+    plt.scatter(new_fish[0], new_fish[1], marker = '^')
+    plt.xlabel("Length")
+    plt.ylabel("Weight")
+    plt.show()
+
+
+
+if __name__ == '__main__' :
+    main()
